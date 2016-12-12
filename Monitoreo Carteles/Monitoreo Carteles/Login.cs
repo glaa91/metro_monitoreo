e29c5458444aebee8763ec07e46e78b0ed1729bb
Gaston Avanses <glaa91@gmail.com>
2016-12-12 16:29:15 -0300

    Se agrego form Logueo Se verifica la informacion para logueo (usuario y password) Se creo funcion Log(tipo, texto) -> aun en prueba

--- Monitoreo Carteles/Monitoreo Carteles/Login.cs
+++ Monitoreo Carteles/Monitoreo Carteles/Login.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 using Npgsql;
 using System.Timers;
 using System.Threading;
@@ -15,63 +15,36 @@ using System.Globalization;
 
 namespace Monitoreo_Carteles
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        /*private static System.Timers.Timer aTimer;
+        private static System.Timers.Timer aTimer;
         private static List<Usuario> usuarios_l = new List<Usuario>();
         private static List<Cartel> carteles_l = new List<Cartel>();
         private static List<Estacion> estaciones_l = new List<Estacion>();
-        private static RichTextBox richTextBox;
-        private TabControl tabControl1;
-        private string tabMonitor_s = "MONITOR", tabDebug_s = "DEBUG", tabConfig_s = "CONFIGURACION";
-        private static Boolean coneccionBaseDatos_b = false;*/
+        //private static RichTextBox richTextBox;
+        //private TabControl tabControl1;
+        //private string tabMonitor_s = "MONITOR", tabDebug_s = "DEBUG", tabConfig_s = "CONFIGURACION";
+        private static Boolean coneccionBaseDatos_b = false;
 
-        public Form1()
+        public Login()
         {
-            /*richTextBox = new RichTextBox();
+            InitializeComponent();
             SetTimer();
+            label_estadoConexion = new Label();
+            
             //levanto por primera vez todo
             coneccionBaseDatos_b = loadServer();
-            if (!coneccionBaseDatos_b)
+            /*if (!coneccionBaseDatos_b)
             {
                 // como no me pude conectar, tengo que esperar a que el el timer se termine y trate de cargarlo el.
                 MessageBox.Show("ERROR al conectarse a la base de datos... /n reintentando en 60 segundos...");
-            }
-            InitializeComponent();
+                label_estadoConexion.Text = "ERROR al conectarse a la base de datos...";
+
+            }*/
             Log("DEBUG", "HOLA");
-            */
+            //label_estadoConexion.Text = "Coneccion EXITOSA a la base de datos...";
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            /*this.tabControl1 = new TabControl();
-            
-            TabControl tabMonitorControl = new TabControl();
-
-            TabPage tab_monitor = new TabPage();
-            TabPage tab_debug = new TabPage();
-            TabPage tab_config = new TabPage();
-
-            tab_monitor.Text = tabMonitor_s;    //le pongo el texto a cada tab
-            tab_debug.Text = tabDebug_s;
-            tab_config.Text = tabConfig_s;
-
-            //richTextBox = new RichTextBox();
-            tab_debug.Controls.Add(richTextBox);
-
-            TabPage[] tabPages = { tab_monitor, tab_debug, tab_config };
-
-            this.tabControl1.SizeMode = TabSizeMode.FillToRight;    // Sizes the tabs so that each row fills the entire width of tabControl1.
-            this.tabControl1.Dock = DockStyle.Fill;     //hago que se llene con el tamaño de la ventana
-            this.tabControl1.Padding = new Point(15, 4);    //les deja un espacion entre vertical y horizontal
-            this.tabControl1.Controls.AddRange(new Control[] {tab_monitor, tab_debug, tab_config}); //agrego las pestañas
-            this.tabControl1.Location = new Point(0, 0);    //seteo en la posicion 0,0
-            this.Controls.Add(tabControl1);
-
-            
-            */
-        }
-        /*
         private static void SetTimer()
         {
             // Create a timer with a 60 second interval.
@@ -117,19 +90,19 @@ namespace Monitoreo_Carteles
                                 //string password = reader.GetString(1);    //password
                                 //Console.WriteLine(username); //username
                                 //Console.WriteLine(password); //password
-                                //
-                                 // lo que hago es cargar los datos de los usuarios la clase. y para independizarme
-                                // lo hago dentro una List, asi con solo hacer un Count se el tamaño total.
-                                //
+                                /*
+                                 * lo que hago es cargar los datos de los usuarios la clase. y para independizarme
+                                 * lo hago dentro una List, asi con solo hacer un Count se el tamaño total.
+                                */
                                 var temp = new Usuario(reader.GetString(0), reader.GetString(1));
                                 usuarios_l.Add(temp);
                             }
-                            for(int i=0; i<usuarios_l.Count; i++)
+                            /*for(int i=0; i<usuarios_l.Count; i++)
                             {
                                 Console.WriteLine("Usuario [{0}]: {1}", i, usuarios_l[i].Username);
                                 Console.WriteLine("Password [{0}]: {1}", i, usuarios_l[i].Password);
                             }
-                            Console.WriteLine();
+                            Console.WriteLine();*/
                         }
                         //LEO TODAS LAS COLUMNAS DE ESTACIONES
                         cmd.CommandText = "SELECT * FROM v_estaciones";
@@ -141,7 +114,7 @@ namespace Monitoreo_Carteles
                                     reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetBoolean(6));
                                 estaciones_l.Add(temp);
                             }
-                            for (int i = 0; i < estaciones_l.Count; i++)
+                            /*for (int i = 0; i < estaciones_l.Count; i++)
                             {
                                 Console.WriteLine("numeroSerie [{0}]: {1}", i, estaciones_l[i].NumeroSerie);
                                 Console.WriteLine("numeroCartel [{0}]: {1}", i, estaciones_l[i].DateTime);
@@ -151,7 +124,7 @@ namespace Monitoreo_Carteles
                                 Console.WriteLine("dateTime [{0}]: {1}", i, estaciones_l[i].DateTime);
                                 Console.WriteLine("ping [{0}]: {1}", i, estaciones_l[i].Ping);
                                 Console.WriteLine();
-                            }
+                            }*/
                         }
                         //LEO TODAS LAS COLUMNAS DE CARTELES
                         cmd.CommandText = "SELECT * FROM v_carteles";
@@ -164,7 +137,7 @@ namespace Monitoreo_Carteles
                                     reader.GetBoolean(7), reader.GetBoolean(8), reader.GetString(9));
                                 carteles_l.Add(temp);
                             }
-                            for (int i = 0; i < carteles_l.Count; i++)
+                            /*for (int i = 0; i < carteles_l.Count; i++)
                             {
                                 Console.WriteLine("numeroSerie [{0}]: {1}", i, carteles_l[i].NumeroSerie);
                                 Console.WriteLine("dateTime [{0}]: {1}", i, carteles_l[i].DateTime);
@@ -177,12 +150,12 @@ namespace Monitoreo_Carteles
                                 Console.WriteLine("fuente5pic [{0}]: {1}", i, carteles_l[i].Fuente5pic);
                                 Console.WriteLine("mensaje [{0}]: {1}", i, carteles_l[i].Mensaje);
                                 Console.WriteLine();
-                            }
+                            }*/
                         }
                     }
                     conexion.Close();
                 }
-                catch (Exception e1)
+                catch (Exception)
                 {
                     MessageBox.Show("Conexion a la base de datos: ERROR", current.ToString());
                     conexion.Close();
@@ -192,24 +165,107 @@ namespace Monitoreo_Carteles
             return estado;
         }
 
-        private void tabPage_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void eventLog1_EntryWritten(object sender, System.Diagnostics.EntryWrittenEventArgs e)
-        {
-
-        }
         public static void Log(string type, string logMessage)
         {
-            StreamWriter w = File.AppendText("log.txt");
-            DateTime dateValue = new System.DateTime();
-            string texto = String.Format("{0} - {1}-{2}", dateValue.ToString("G"), type, logMessage);
-            w.WriteLine(texto);
-            Console.WriteLine(texto);
-            richTextBox.AppendText(System.Environment.NewLine + texto);
-        }*/
+            DateTime dateValue = DateTime.Now;
+            string texto = String.Format("{0} - {1} - {2}", dateValue.ToString("G"), type, logMessage); // la G indica el formato de la hora XX/XX/XX 12:12:12
+            string path = "log.txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("ARCHIVO CREADO!");
+                }
+            }
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(texto);
+                Console.Write(texto);
+            }
+            //richTextBox.AppendText(System.Environment.NewLine + texto);
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            bool usuario_b = false;
+            string username_s, password_s;
+            if (!string.IsNullOrWhiteSpace(textBox_username.Text) && !string.IsNullOrWhiteSpace(textBox_password.Text))
+            {   //veo que no esten vacio los text box
+                username_s = textBox_username.Text;
+                password_s = textBox_password.Text;
+                for (int i = 0; i < usuarios_l.Count; i++) //busco en mis objetos usuarios cargados para comparar
+                {
+                    if (usuarios_l[i].Username == username_s && usuarios_l[i].Password == password_s) // valido usuario y contraseña con lo que tengo cargado
+                    {
+                        //USUARIO VALIDADO!
+                        usuario_b = true;
+                    }
+                }
+            }
+            if (coneccionBaseDatos_b)
+            {
+                if (usuario_b)
+                {
+                    MessageBox.Show("USUARIO VALIDO!", "Inicio sesion");
+                }
+                else
+                {
+                    MessageBox.Show("USUARIO INVALIDO! - Verifique usuario/contraseña", "Inicio sesion");
+                }
+            }
+            else
+            {
+                MessageBox.Show("AUN NO SE HA CONECTADO A LA BASE DE DATOS...","Error de conexion a la base de datos");
+            }
+        }
+
+        private void textBox_username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
-
